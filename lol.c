7f715743a20a4e869c01af835e80ac968f68b0b5
Booker Robinson <robinson.booker11@gmail.com>
2021-06-06 12:30:10 -0400

    fuck saving shit

--- lol.c
+++ lol.c
@@ -2,6 +2,14 @@
 
 typedef int lolnumber;
 
+typedef void summon;
+
+
 lolnumber main(){
     printf("Lmaooo");
+
+}
+
+summon shit(){
+    printf("LOLLLL P=NP");
 }
---
+++ poop.java
@@ -0,0 +1,21 @@
+interface BookerTemplate{
+
+    void hair();
+    void occupation();
+    void pet();
+
+}
+
+public class Booker implements BookerTemplate{
+
+    public static void main(String args[]){
+
+
+    }
+    public void hair(){
+        Scanner obj = new Scanner(System.in);
+        System.out.println();
+        int 
+    }
+
+}
