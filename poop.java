bf9e2930b41892c1c4c42b7b34c28f8d9bc50d2c
Booker Robinson <robinson.booker11@gmail.com>
2021-06-06 12:43:31 -0400

    nothing

--- poop.java
+++ poop.java
@@ -1,21 +1,33 @@
+import java.util.Scanner;
+
 interface BookerTemplate{
 
-    void hair();
-    void occupation();
-    void pet();
+    void hair(String x);
+    void occupation(String c);
+    void pet(String p);
 
 }
 
 public class Booker implements BookerTemplate{
 
+
     public static void main(String args[]){
+        Booker obj = new Booker();
+        obj.hair("Blue");
+        obj.pet("Balls");
+        obj.occupation("Stripper");
+    }
+    @Override
+    public void hair(String x){
+        System.out.println("bookers hair is:"+" "+x);
+    }
+    public void occupation(String c){
+        System.out.println("Bookers occupation is:"+" "+c);
+    }
+    public void pet(String p){System.out.println("Bookers pet is:"+" "+p);};
+
+  
 
 
-    }
-    public void hair(){
-        Scanner obj = new Scanner(System.in);
-        System.out.println();
-        int 
-    }
 
 }
